c2dbed7b788da35205cea14088c16300ac5ffdec
Devika <spmdevika@gmail.com>
2020-10-21 21:07:04 +0530

    Create a service object to extract logic from the get_posts method

--- app/javascript/packs/posts/infinite_scroll.js
+++ app/javascript/packs/posts/infinite_scroll.js
@@ -1,17 +1,19 @@
-$(document).ready('turbolinks:load', function() {
+$(document).ready(function() {
+    console.log("Hello");
     var isLoading = false;
-    if ($('.infinite-scroll', this).size() > 0) {
-      $(window).on('scroll', function() {
-        var more_posts_url = $('.pagination a.next_page').attr('href');
-        var threshold_passed = $(window).scrollTop() > $(document).height() - $(window).height() - 60;
-        if (!isLoading && more_posts_url && threshold_passed) {
-          isLoading = true;
-          $.getScript(more_posts_url).done(function (data,textStatus,jqxhr) {
-            isLoading = false;
-          }).fail(function() {
-            isLoading = false;
-          });
-        }
+    if ($('.infinite-scroll', this).length > 0) {
+        $(parent.window.document).scroll(function() {
+            var more_posts_url = $('.pagination a.next_page').attr('href');
+            var threshold_passed = $(window).scrollTop() > $(document).height() - $(window).height() - 60;
+            console.log(more_posts_url+" "+threshold_passed);
+            if (!isLoading && more_posts_url && threshold_passed) {
+                isLoading = true;
+                $.getScript(more_posts_url).done(function (data,textStatus,jqxhr) {
+                    isLoading = false;
+                }).fail(function() {
+                    isLoading = false;
+                });
+            }
       });
     }
   });
