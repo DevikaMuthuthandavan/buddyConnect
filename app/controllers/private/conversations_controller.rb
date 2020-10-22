74de62c972b14e56d3c6703a3cc5c1d26a57d33f
Devika <spmdevika@gmail.com>
2020-10-22 20:48:18 +0530

    Unknown format Error resolved

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,6 +1,8 @@
 class Private::ConversationsController < ApplicationController
     skip_before_action :verify_authenticity_token, :only => :create
+
     def create
+        @post = Post.find(params[:post_id])
         recipient_id = Post.find(params[:post_id]).user.id
         conversation = Private::Conversation.new(sender_id: current_user.id, 
                                                  recipient_id: recipient_id)
@@ -9,11 +11,12 @@ class Private::ConversationsController < ApplicationController
                                   conversation_id: conversation.id, 
                                   body: params[:message_body])
           respond_to do |format|
-            format.js {render: "posts/show/contact_user/message_form/success"}
+            format.js {render partial: 'posts/show/contact_user/message_form/success.js.erb'}
+            format.html {redirect_to root_path};
           end
         else
           respond_to do |format|
-            format.js {render: "posts/show/contact_user/message_form/fail"}
+            format.js {render partial: 'posts/show/contact_user/message_form/fail.js'}
           end
         end
       end
