33c51149a137b00a6fffe0286350e601fcd13cc8
Sruthilaya <sruthilaya.vss@gmail.com>
2020-10-23 20:52:10 +0530

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversations - Define a private_conv_recipient helper method in the   private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model   and add specs for it - Create _heading.html.erb and _messages_list.html.erb files   inside the private/conversations/conversation - Define a load_private_messages in private/conversations_helper.rb   and add specs for it - Create a _new_message_form.html.erb inside the   private/conversations/conversation - Create a _open.js.erbinside private/conversations - Create a  position_and_visibility.js inside the   assets/javascripts/conversations - Create a  conversation_window.scss inside the   assets/stylesheets/partials - Define an opened_conversations_windows helper method in   ApplicationController - Create a _private_conversations_windows.html.erb inside the   layouts/application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :private do 
+    resources :conversations, only: [:create] do
+      member do
+        post :close
+      end
+    end
+    resources :messages, only: [:index, :create]
+  end
+  
     devise_scope :user do
       get 'login', to: 'devise/sessions#new'
     end
@@ -16,13 +25,5 @@ Rails.application.routes.draw do
       get 'team'
     end
   end
-  
-  namespace :private do 
-    resources :conversations, only: [:create] do
-      member do
-        post :close
-      end
-    end
-    resources :messages, only: [:index, :create]
-  end
+
 end
