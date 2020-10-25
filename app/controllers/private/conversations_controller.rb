64e4f01e31f0e9a46c05ed9994dfe54d58c59777
Devika <spmdevika@gmail.com>
2020-10-25 12:46:14 +0530

    Make the close conversation button functional

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,6 +1,6 @@
 class Private::ConversationsController < ApplicationController
     skip_before_action :verify_authenticity_token, :only => :create
-
+    
     def create
         @post = Post.find(params[:post_id])
         recipient_id = Post.find(params[:post_id]).user.id
@@ -21,6 +21,16 @@ class Private::ConversationsController < ApplicationController
         end
         add_to_conversations unless already_added?
       end
+      
+      def close
+        @conversation_id = params[:id].to_i
+        session[:private_conversations].delete(@conversation_id)
+      
+        respond_to do |format|
+          format.js
+          format.html {redirect_to root_path};
+        end
+      end
 
       private
 
@@ -31,5 +41,5 @@ class Private::ConversationsController < ApplicationController
 
         def already_added?
             session[:private_conversations].include?(@conversation.id)
-          end
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/private/conversations/:id/close', to: 'private/conversations#close'
+
   namespace :private do 
     resources :conversations, only: [:create] do
       member do
